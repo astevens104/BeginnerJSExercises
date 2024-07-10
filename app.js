624688d0dc78708f8baa004efd25f399dd28b7e7
astevens104 <agstevens104@gmail.com>
2024-07-10 13:38:58 -0700

    Beginner JS exercises

---
+++ app.js
@@ -0,0 +1,63 @@
+// Question 1 - add two numbers
+
+// function addition(num1, num2) {
+//     return num1 + num2;
+// }
+
+// let sum = addition(7, 3)
+// console.log('The sum is ', sum)
+
+// question 2 - convert hours into seconds
+
+// 
+// function hoursIntoSeconds(hour) {
+//     return hour * 3600;
+// }
+
+
+// console.log(hoursIntoSeconds(24));
+
+// Question 3 - Calculate the perimeter of a rectangle
+
+// function calcPerimeter(length, width) {
+//     return (length * 2) + (width * 2);
+// }
+
+// console.log(calcPerimeter(2, 9))
+
+// question 4 - calculate area of a triangle
+
+// function calcTriangleArea(base, height) {
+//     return base * height * .5;
+// }
+
+// console.log(calcTriangleArea(20, 20));
+
+// Question 5 - Extend a string
+
+// function extendString(string) {
+//     return string + "Frontend"
+// }
+
+// console.log(extendString('Orange'))
+
+// Question 6 - Greater than 100?
+
+// Because a mathematical statement returns a boolean
+// an if statement is not necessary
+
+// function sumGreaterThan100(num1, num2){
+//     return num1 + num2 > 100
+// }
+
+// console.log(sumGreaterThan100(50, 53))
+
+// Question 7 - less than or equal to zero?
+
+// function lessThanOrEqualToZero (num1) {
+//     return num1 <= 0
+// }
+
+// console.log(lessThanOrEqualToZero(0))
+
+// Question 8 - 
